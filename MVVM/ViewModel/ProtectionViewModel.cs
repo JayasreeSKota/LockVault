634c795998ea031f3dab1b8ea442dc28bb23178a
JayasreeSKota <jayasreeskota@gmail.com>
2023-05-16 20:49:37 +0530

    first commit

---
+++ MVVM/ViewModel/ProtectionViewModel.cs
@@ -0,0 +1,93 @@
+﻿using ModernVPN.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ModernVPN.MVVM.ViewModel
+{
+   internal class ProtectionViewModel: ObservableObject
+    {
+        public ObservableCollection<ServerModel> Servers { get; set; }
+
+        private string _connectionStatus;
+        public string ConnectionStatus
+        {
+            get { return _connectionStatus; }
+            set { 
+                _connectionStatus = value;
+                OnPropertyChanged();
+                  }
+        }
+
+        public RelayCommand ConnectCommand { get; set; }
+        public ProtectionViewModel()
+        {
+            Servers = new ObservableCollection<ServerModel>();
+            for(int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country = "USA"
+                });
+            }
+            ConnectCommand =  new RelayCommand(o => { 
+                ConnectionStatus = "Connecting..";
+                var process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+                process.StartInfo.ArgumentList.Add(@"/c rasdial MyServer vpnbook 3ev7r8m/ phonebook:us1.vpnbook.com");
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+                process.WaitForExit();
+
+                switch (process.ExitCode)
+                {
+                    case 0:
+                        Debug.WriteLine("Success!");
+                        ConnectionStatus = "Connected!";
+                        break;
+                    case 691:
+                        Debug.WriteLine("Wrong credentials!");
+                        ConnectionStatus = "Wrong credentials!";
+                        break;
+                    default:
+                        Debug.WriteLine($"Error:{process.ExitCode}");
+                        break;
+                 }
+            });
+            
+        }
+        private void ServerBuilder()
+        {
+            var address = "us1.vpnbook.com";
+            var FolderPath = $"{Directory.GetCurrentDirectory()}/VPN";
+            var PbkPath = $"{FolderPath}/{address}.pbk";
+
+            if (!Directory.Exists(FolderPath))
+                Directory.CreateDirectory(FolderPath);
+
+            if (File.Exists(PbkPath))
+            {
+                MessageBox.Show("Connection already exists!");
+                return;
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine("[MyServer]");
+            sb.AppendLine("MEDIA=rastapi");
+            sb.AppendLine("Port=VPN2-0");
+            sb.AppendLine("Device=WAN Miniport (IKEV2)");
+            sb.AppendLine("Device=vpn");
+            sb.AppendLine($"PhoneNumber={address}");
+            File.WriteAllText(PbkPath, sb.ToString());
+        }
+    }
+}
---
+++ MVVM/ViewModel/ServerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ModernVPN.MVVM.ViewModel
+{
+    public class ServerModel
+    {
+        public int ID{ get; set; }
+        public string Username { get; set;}
+        public string Password { get; set; }
+        public string Server { get; set; }
+        public string Country { get; set; }
+
+
+    }
+}
